d28645fca3a4008ac31731e0a619e38143b2744f
Artur Nizamov <mreclipse27@gmail.com>
2020-08-07 11:24:33 +0500

    synced all project files

---
+++ mod3_lesson2_step13.py
@@ -0,0 +1,87 @@
+import unittest
+from selenium import webdriver
+import time
+
+
+class TestAbs(unittest.TestCase):
+    def test_regpage_one(self):
+        try:
+            link = "http://suninjuly.github.io/registration1.html"
+            browser = webdriver.Chrome()
+            browser.get(link)
+            required_welcome_text = "Congratulations! You have successfully registered!"
+
+            # Код, который заполняет обязательные поля
+            input1 = browser.find_element_by_css_selector(".first_block .form-control.first")
+            input1.send_keys("Ivan")
+            input2 = browser.find_element_by_css_selector(".first_block .form-control.second")
+            input2.send_keys("Petrov")
+            input3 = browser.find_element_by_css_selector(".first_block .form-control.third")
+            input3.send_keys("123@abc.abc")
+
+            # Отправляем заполненную форму
+            button = browser.find_element_by_css_selector("button.btn")
+            button.click()
+
+            # Проверяем, что смогли зарегистрироваться
+            # ждем загрузки страницы
+            time.sleep(1)
+
+            # находим элемент, содержащий текст
+            welcome_text_elt = browser.find_element_by_tag_name("h1")
+            # записываем в переменную welcome_text текст из элемента welcome_text_elt
+            welcome_text = welcome_text_elt.text
+
+            # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+            self.assertEqual(welcome_text, required_welcome_text, 'Text incorrect')
+
+        finally:
+            # ожидание чтобы визуально оценить результаты прохождения скрипта
+            time.sleep(10)
+            # закрываем браузер после всех манипуляций
+            browser.quit()
+
+        # self.assertEqual(abs(-42), 42, "Should be absolute value of a number")
+
+    def test_regpage_two(self):
+        try:
+            link = "http://suninjuly.github.io/registration2.html"
+            browser = webdriver.Chrome()
+            browser.get(link)
+            required_welcome_text = "Congratulations! You have successfully registered!"
+
+            # Код, который заполняет обязательные поля
+            input1 = browser.find_element_by_css_selector(".first_block .form-control.first")
+            input1.send_keys("Ivan")
+            input2 = browser.find_element_by_css_selector(".first_block .form-control.second")
+            input2.send_keys("Petrov")
+            input3 = browser.find_element_by_css_selector(".first_block .form-control.third")
+            input3.send_keys("123@abc.abc")
+
+            # Отправляем заполненную форму
+            button = browser.find_element_by_css_selector("button.btn")
+            button.click()
+
+            # Проверяем, что смогли зарегистрироваться
+            # ждем загрузки страницы
+            time.sleep(1)
+
+            # находим элемент, содержащий текст
+            welcome_text_elt = browser.find_element_by_tag_name("h1")
+            # записываем в переменную welcome_text текст из элемента welcome_text_elt
+            welcome_text = welcome_text_elt.text
+
+            # с помощью assertEqual проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+            self.assertEqual(welcome_text, required_welcome_text, 'Text incorrect')
+
+        finally:
+            # ожидание чтобы визуально оценить результаты прохождения скрипта
+            time.sleep(10)
+            # закрываем браузер после всех манипуляций
+            browser.quit()
+
+            # self.assertEqual(abs(-42), 42, "Should be absolute value of a number")
+
+
+if __name__ == "__main__":
+    unittest.main()
